dcc2974c1bae5b60ac62b8cb12a416dd79301542
Sergio Cuéllar <s.cuellar.21s@gmail.com>
2017-01-17 21:52:18 -0600

    -CODIGO JAVASCRIPT Y  ESTILOS CSS    *PERFECTOS!*

--- JS PERFECTO FUNCIONAL!.js
+++ JS PERFECTO FUNCIONAL!.js
@@ -7,20 +7,20 @@ $(document).ready(function(){
 	//$('#centro').height(screen.height - $("#defaultNavbar1").height());  //"screen.height" SI FUNCIONA (type NUMERO) "height"(variable) (type STRING) NO FUNCIONA 
 	//document.getElementById("demo").innerHTML = screen.height - $("#defaultNavbar1").height();
 	
-	
-	var HeaderOffset = $('#principales').offset().top;        //en lugar de     pageYOffset
+	var HeaderOffset = $('#principales').offset().top,        //en lugar de     pageYOffset
+			LogoFormOffset = $('main').offset().top,
+			SobreMiOffset = $('#SobreMi').offset().top,
+			ContactoOffset = $('#Contacto').offset().top;
 	$(window).scroll(function(){
+		// para fijar la barra de #principales
 		if ($(window).scrollTop() >= HeaderOffset){
 			$('#header').addClass('fixed');
 		}
 		else{
 			$('#header').removeClass('fixed');
 		}
-	});
-	
-	var LogoFormOffset = $('main').offset().top;      //menos 64px!! (altura de la barra)   DEPENDIENDO DEL EFECTO QUE QUIERA
-	$(window).scroll(function(){
-		if ($(window).scrollTop() >= LogoFormOffset){
+		// para el LOGO y FORM se fijen a la barra
+		if ($(window).scrollTop() >= LogoFormOffset){  //menos 64px!! (altura de la barra)   DEPENDIENDO DEL EFECTO QUE QUIERA
 			$('.navbar-form').addClass('fixed');
 			$('.navbar-header').addClass('fixed');
       //$('#principales').css("background-color", "MISMO PERO SIN TRASPARENCIA");
@@ -30,11 +30,7 @@ $(document).ready(function(){
 			$('.navbar-header').removeClass('fixed');
 			//$('#principales').css("background-color", "CON TRASPARENCIA");
 		}
-	});
-	
-	var SobreMiOffset = $('#SobreMi').offset().top;
-	var ContactoOffset = $('#Contacto').offset().top;
-	$(window).scroll(function(){
+		// para ajustar las secciones cuando se SALTE a ellas   (OPCIONAL)
 		if ($(window).scrollTop() >= SobreMiOffset && $(window).scrollTop() <= ContactoOffset){
 			$('#Contacto').removeClass("ClaseOnScroll");
 			$('#SobreMi').addClass("ClaseOnScroll");
@@ -46,29 +42,11 @@ $(document).ready(function(){
 		else{
 			$('#SobreMi').removeClass("ClaseOnScroll");
 			$('#Contacto').removeClass("ClaseOnScroll");
-		}
-	});//INSERVIBLE SI NO SE USA ".ClaseOnScroll"
-	
-	
-	
+		}//INSERVIBLE SI NO SE USA ".ClaseOnScroll"
+	});
+		
 	//AFFIX      FUNCIONA PERO CONVIENE USAR LA FUNCION DE ARRIBA
 	//$("#principales").affix({offset: {top: $("#principales").offset().top} });
-	
-	
-	//ACTIVE     FUNCIONA PERO NO CON LOS PRINCIPALES     CREO QUE POR LAS REGLAS CSS ESTABLECIDAS(QUITARLAS PARA PROBAR)                     DEBE SER RELAVIVE(li = psition:relative) PARA FUNCIONAR
-	//$('.nav li').click(function(event){
-//		$('.active').removeClass('active');
-//		$(this).addClass('active');
-//	});
-	
-	//$('.nav li').click(function(e) {
-//		var $this = $(this);
-//		if (!$this.hasClass('active')) {
-//			$this.addClass('active');
-//		}
-//		e.preventDefault();
-//	});
-
 	
 	// SROLLSPY  FUNCION DE   W3.COM
   // Add scrollspy to <body>
@@ -100,4 +78,20 @@ $(document).ready(function(){
     }  // End if
   });
 	
+	
+	
+	//ACTIVE            DEBE SER RELAVIVE(li = psition:relative) PARA FUNCIONAR
+	//$('.nav li').click(function(event){
+//		$('.active').removeClass('active');
+//		$(this).addClass('active');
+//	});
+	
+	//$('.nav li').click(function(e) {
+//		var esteElemento = $(this);
+//		if (!esteElemento.hasClass('active')) {
+//			esteElemento.addClass('active');
+//		}
+//		e.preventDefault();
+//	});
+
 });
